4017fedfaabd207969a72aa942a5caaaf6a182ee
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-03-07 16:06:13 +0000

    Add outlet for rendering child pages

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,30 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/events"
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Events
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className={classes.header}>
+        <nav>
+          <ul className={classes.list}>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/events"
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+              >
+                Events
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Outlet />
+    </>
   );
 }
 
