e5333cf3b6b67522568bd4d40805be481d8d48ad
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-03-07 16:06:31 +0000

    Add dummy events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,6 @@ import Events from "./pages/EventsPage";
 import Home from "./pages/HomePage";
 import NewEvent from "./pages/NewEventPage";
 
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 const router = createBrowserRouter([
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
+const EVENTS = [
+  { id: 1, name: "Carnival" },
+  { id: 2, name: "Christmas" },
+  { id: 3, name: "Library's 60th Birthday" },
+];
+
 export default function Events() {
-  return <h1>Events</h1>;
+  return (
+    <>
+      <h1>Events</h1>
+      <ul>
+        {EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
