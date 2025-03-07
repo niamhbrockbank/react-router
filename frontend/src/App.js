ca66cce99e219c549a09bd82f3afbc986add9bcb
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-03-07 15:43:25 +0000

    Add dummy pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
 // Challenge / Exercise
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
 // 2. Add routing & route definitions for these five pages
 //    - / => HomePage
 //    - /events => EventsPage
