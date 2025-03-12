80fe5453051a23e0ad1e11e857d8fec9c67f4d14
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-03-12 15:38:45 +0000

    Add custom error message to when cant access events

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,10 @@ const router = createBrowserRouter([
               const response = await fetch("http://localhost:8080/events");
 
               if (!response.ok) {
-                // TODO
+                throw new Response(
+                  JSON.stringify({ message: "Could not fetch events" }),
+                  { status: 500 }
+                );
               } else {
                 const resData = await response.json();
                 return resData.events;
