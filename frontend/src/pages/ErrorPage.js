19a92868e4c2fcddc180b9460ad70d7c8e2fa93b
Niamh Brockbank <niamh.brockbank@openbet.com>
2025-03-12 15:47:17 +0000

    Add generic error page

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,10 +1,27 @@
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 import PageContent from "../components/PageContent";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = <p>Something went wrong!</p>;
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found!";
+    message = "Could not find resource or page.";
+  }
+
   return (
-    <PageContent title="An error occurred!">
-      <p>Something went wrong!</p>
-    </PageContent>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>{message}</PageContent>
+    </>
   );
 }
 
